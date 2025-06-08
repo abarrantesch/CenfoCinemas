5f61b714a4caaf9a9463d26b187219423c8cf321
ajosu <ajosu@DESKTOP-8A3KP44>
2025-06-07 22:06:01 -0600

    Programa CenfoCinemas. Falta agreegar acceso a datos y configurar tablas con Sql

---
+++ DTOs/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class User : BaseDTO
+    {
+
+        public string UserCode { get; set; }
+        public string Name { get; set; }
+        public  string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Status { get; set; }
+
+
+
+    }
+}
---
+++ DataAccess/DAO/SqlDao.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+
+    /// Clase u objeto que se encarga de la comunicacion de la base de datos. Solo ejecuta Store Procedures. Patron Singleton opara asegugar una unica instancia de la clase SqlDao por aplicacion.
+    public class SqlDao
+    {
+
+        // 1. Crea una instancia privada esttica de la clase SqlDao.
+        private static SqlDao _instance;
+
+        private string _connectionString;
+
+        //2. Redefine el constructor por defecto y convertir en privado
+
+        private SqlDao()
+        {
+            _connectionString = string.Empty;
+        }
+
+        public static SqlDao getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SqlDao();
+            }
+            return _instance;
+        }
+
+        //Funcion de ejecucion de SP sin returno de datos
+        public void ExecuteProcedure(SqlOperation operation) 
+        {
+            //conectar a la DB
+            //Ejecutar el SP
+        }
+
+        //Funcion de ejecucion de SP con retorno de datos
+        public List<Dictionary<string, object>> ExecuteQueryProcedure(SqlOperation operation) 
+        {
+            //conectar a la DB
+            //Ejecutar el SP y retornar los datos
+            //convertuirlo en DTOs
+            
+            var List= new List<Dictionary<string, object>>();
+
+            return List;
+        }
+
+
+    }
+}
